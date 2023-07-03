234be5c7703dd3f3650bc898c71bab29a75ece6e
Simon Stanford <simonjstanford@gmail.com>
2023-07-03 07:15:57 +0100

    Added some basic UI for creating/removing months and expenses

--- client/src/app/components/user.expenses/user.expenses.component.ts
+++ client/src/app/components/user.expenses/user.expenses.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from 'src/app/models/user';
-import { UserExpenses } from 'src/app/models/userExpenses';
+import { Expense, ExpenseMonth, UserExpenses } from 'src/app/models/userExpenses';
 import { ApiService } from 'src/app/services/api.service';
 import { OauthServiceService } from 'src/app/services/oauth.service';
 
@@ -41,7 +41,57 @@ export class UserExpensesComponent {
         console.log(resp);
       });
     } else {
-      console.log("No expenses to save!")
+      console.log("No expenses to save!");
     }
+  }
+
+  public addExpense(data: Expense[]) {
+    if (!data) {
+      console.log("No list to add expense!");
+      return;
+    }
+
+    data.push({
+      name: "New Item",
+      value: 0,
+    });
+  }
+
+  public addMonth() {
+    if (!this.expenses) {
+      console.log("No list to add month!");
+      return;
+    }
+
+    const month: ExpenseMonth = this.createNewMonth();
+
+    if (!this.expenses.months) {
+      this.expenses.months = [month];
+    } else {
+      this.expenses.months.push(month);
+    }
+  }
+
+  public removeMonth(month: ExpenseMonth) {
+    if (!this.expenses) {
+      console.log("No list to remove month!");
+      return;
+    }
+
+    const index = this.expenses.months.indexOf(month, 0);
+    if (index > -1) {
+      this.expenses.months.splice(index, 1);
+    }
+  }
+
+  private createNewMonth(): ExpenseMonth {
+    const currentMonth = new Date();
+    const firstDay = new Date(Date.UTC(currentMonth.getFullYear(), currentMonth.getMonth(), 1));
+
+    return {
+      monthStart: firstDay,
+      income: [],
+      outgoings: []
+    };
   }
 }
